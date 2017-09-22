7800d77a957417c5af1a897c48f0bfebd0a1880b
S Stewart <iamtheworst@programmer.net>
2017-09-21 21:02:38 -0500

    basics[1,2]: Hello World/Number Crunch

---
+++ the-basic/number-crunch.ex
@@ -0,0 +1,13 @@
+first = 5 * 4
+second = 6 * 9
+third = 4 * 2 * 0 * 20
+fourth = 9 / 7
+
+IO.puts "5 * 4"
+IO.puts first
+IO.puts "6 * 9"
+IO.puts second
+IO.puts "4 * 2 * 0 * 20"
+IO.puts third
+IO.puts "9 / 7"
+IO.puts fourth
